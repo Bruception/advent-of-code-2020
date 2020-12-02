13db6b287855cc7b3bd987874d156c7c6f82ecd2
Bruce Berrios <bberr022@fiu.edu>
2020-12-02 16:21:55 -0500

    Update init_day

--- init_day.sh
+++ init_day.sh
@@ -1,1 +1,3 @@
-dir=day$1 ; mkdir $dir ; touch $dir/input.txt $dir/part1.py $dir/part2.py
+next_day=`ls | grep '^day' | wc -l`
+dir=day$(( $next_day + 1 ))
+mkdir $dir ; touch $dir/input.txt $dir/part1.py $dir/part2.py
