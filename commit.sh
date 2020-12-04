ffc2e0b1b9b087c6604b08378c506484de082fd4
Bruce Berrios <bberr022@fiu.edu>
2020-12-04 12:19:12 -0500

    Fix commit.sh

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 day=`git status --short | sed 's/?? //g' | grep 'day'`
-dayNumber=`echo $day | egrep -oh '[0-9]'`
+dayNumber=`echo $day | egrep -oh '[0-9]+'`
 git add $day
 git commit -m "Complete day $dayNumber"
 git push -u origin master
