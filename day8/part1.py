07a4f85f2caea572f17c4a8adc705e26b84c1b93
Bruce Berrios <bberr022@fiu.edu>
2020-12-08 11:29:18 -0500

    Complete day 8

--- day8/part1.py
+++ day8/part1.py
@@ -5,8 +5,7 @@ def prepareInstruction(instruction):
     return (opcode, int(value))
 
 file = open(f'{sys.path[0]}/input.txt', 'r')
-instructions = [line[:-1] if line[-1] == '\n' else line for line in file]
-instructions = list(map(prepareInstruction, instructions))
+instructions = [prepareInstruction(line[:-1] if line[-1] == '\n' else line) for line in file]
 
 visited = {}
 accumulator = 0
