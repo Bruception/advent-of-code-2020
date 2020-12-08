16a0da600c7a4e3abdaed9ef9c5d89697f426b28
Bruce Berrios <bberr022@fiu.edu>
2020-12-08 10:54:50 -0500

    Fix bug

--- init_day.sh
+++ init_day.sh
@@ -1,4 +1,4 @@
 next_day=$(( `ls -d */ | egrep '^day[0-9]+/$' | wc -l` + 1 ))
 dir=day$next_day
-mkdir $dir ; $dir/part1.py $dir/part2.py
+mkdir $dir ; touch $dir/part1.py $dir/part2.py
 bash ./get_input.sh $next_day
