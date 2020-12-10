de17f1a8413c444cf7cfeb3e0d762b1a53ca1705
Bruce Berrios <bberr022@fiu.edu>
2020-12-10 10:09:58 -0500

    Fix bug

--- run.sh
+++ run.sh
@@ -6,13 +6,14 @@ printf '          {} {_,_,_,_,}\n'
 printf '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
 
 dayNumber=1
-for day in `ls -d */ | egrep '^day[0-9]+\/$'`
+for day in `ls -d */ | egrep '^day[0-9]+\/$' | sed -e 's/day//g' -e 's/\///g' | sort -n`
 do
     printf "Day $dayNumber\n"
     part=1
-    for script in `ls $day | egrep '^part(1|2)\.py$'`
+    dir=day$day/
+    for script in `ls $dir | egrep '^part(1|2)\.py$'`
     do
-        answer=`python3 $day$script 2>/dev/null || printf "Oops! No answer yet :("`
+        answer=`python3 $dir$script 2>/dev/null || printf "Oops! No answer yet :("`
         printf "\\tAnswer for part $part: $answer\n"
         part=$(( $part + 1 ))
     done
