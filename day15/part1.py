09e67d7ad5bf9b162079e29bc944b95ce7031f77
Bruce Berrios <bberr022@fiu.edu>
2020-12-15 14:47:49 -0500

    Complete day 15

---
+++ day15/part1.py
@@ -0,0 +1,27 @@
+import sys
+
+line = open(f'{sys.path[0]}/input.txt', 'r').readline().split(',')
+turnMap = {}
+turnCount = {}
+lastNum = 0
+for i, num in enumerate(line):
+    n = int(num)
+    turnMap[n] = (i, i)
+    turnCount[n] = 1
+    lastNum = n
+turn = len(turnMap)
+while (turn < 2020):
+    timesSeen = turnCount[lastNum]
+    if (timesSeen <= 1):
+        lastNum = 0
+        turnCount[0] += 1
+        t1, t2 = turnMap[0]
+        turnMap[0] = (t2, turn)
+    else:
+        t1, t2 = turnMap[lastNum]
+        lastNum = t2 - t1
+        turnCount[lastNum] = 1 if lastNum not in turnCount else turnCount[lastNum] + 1
+        t3, t4 = (turn, turn) if lastNum not in turnMap else turnMap[lastNum]
+        turnMap[lastNum] = (t4, turn)
+    turn += 1
+print(lastNum)
