3d98e6ec0b360cddf5bb1cde030f66b593204d89
Bruce Berrios <bberr022@fiu.edu>
2020-12-16 10:23:21 -0500

    Refactor day 15

--- day15/part2.py
+++ day15/part2.py
@@ -1,27 +1,15 @@
 import sys
 
-line = open(f'{sys.path[0]}/input.txt', 'r').readline().split(',')
+line = [int(n) for n in open(f'{sys.path[0]}/input.txt', 'r').readline().split(',')]
 turnMap = {}
-turnCount = {}
-lastNum = 0
 for i, num in enumerate(line):
-    n = int(num)
-    turnMap[n] = (i, i)
-    turnCount[n] = 1
-    lastNum = n
+    turnMap[num] = i
 turn = len(turnMap)
+lastNum, nextNum = line[-1], 0
 while (turn < 30000000):
-    timesSeen = turnCount[lastNum]
-    if (timesSeen <= 1):
-        lastNum = 0
-        turnCount[0] += 1
-        t1, t2 = turnMap[0]
-        turnMap[0] = (t2, turn)
-    else:
-        t1, t2 = turnMap[lastNum]
-        lastNum = t2 - t1
-        turnCount[lastNum] = 1 if lastNum not in turnCount else turnCount[lastNum] + 1
-        t3, t4 = (turn, turn) if lastNum not in turnMap else turnMap[lastNum]
-        turnMap[lastNum] = (t4, turn)
+    prev = turn - 1
+    nextNum = prev - turnMap[lastNum] if lastNum in turnMap else 0
+    turnMap[lastNum] = prev
+    lastNum = nextNum
     turn += 1
 print(lastNum)
