97063c4fab0cf775372908cd2413ca8b68b6667c
Bruce Berrios <bberr022@fiu.edu>
2020-12-17 15:57:12 -0500

    Refactor day 15

--- day15/part2.py
+++ day15/part2.py
@@ -1,15 +1,15 @@
 import sys
 
+maxTurn = 30000000
 line = [int(n) for n in open(f'{sys.path[0]}/input.txt', 'r').readline().split(',')]
-turnMap = {}
+turnMap = [-1] * (maxTurn + 1)
 for i, num in enumerate(line):
-    turnMap[num] = i
-turn = len(turnMap)
+    turnMap[num] = i + 1
+turn = len(line)
 lastNum, nextNum = line[-1], 0
-while (turn < 30000000):
-    prev = turn - 1
-    nextNum = prev - turnMap[lastNum] if lastNum in turnMap else 0
-    turnMap[lastNum] = prev
-    lastNum = nextNum
+while (turn < maxTurn):
+    curr = turnMap[lastNum]
+    turnMap[lastNum] = turn
+    lastNum = 0 if curr == -1 else turn - curr
     turn += 1
 print(lastNum)
