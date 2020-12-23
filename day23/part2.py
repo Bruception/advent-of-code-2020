0d43c8d7a5fd196a502d16f22f88cd15b4a2d754
Bruce Berrios <bberr022@fiu.edu>
2020-12-23 10:43:36 -0500

    Refactor day 23

--- day23/part2.py
+++ day23/part2.py
@@ -1,52 +1,39 @@
 import sys
-import cProfile
 
 class HashedCircularList:
+
     class Node:
         def __init__(self, value):
             self.next = None
-            self.prev = None
             self.value = value
-
-        def setNextPrev(self, next, prev):
-            self.next = next
-            self.prev = prev
 
     def __init__(self, nums):
         self.nodeMap = {}
         self.head = self.Node(None)
-        self.head.setNextPrev(self.head, self.head)
         self.minValue, self.maxValue = 1, 1000000
-        for num in nums:
-            self.addLast(num)
-        last = self.head.prev
-        self.head = self.head.next
-        self.head.prev = last
-        last.next = self.head
+        self.addNums(nums)
 
-    def addLast(self, num):
-        newNode = self.Node(num)
-        self.nodeMap[num] = newNode
-        prevTail = self.head.prev
-        prevTail.next = newNode
-        newNode.setNextPrev(self.head, prevTail)
-        self.head.prev = newNode
+    def addNums(self, nums):
+        current = self.head
+        for num in nums:
+            current.next = self.Node(num)
+            current = current.next
+            self.nodeMap[num] = current
+        current.next = self.head.next
+        self.head = self.head.next
 
     def getNextThree(self, value):
         currentNode = self.nodeMap[value]
         nextNode = currentNode.next.next.next.next
         threeCups = currentNode.next
         currentNode.next = nextNode
-        nextNode.prev = currentNode
         return threeCups
 
     def addThreeAt(self, value, threeCups):
         destNode = self.nodeMap[value]
         prevNext = destNode.next
         destNode.next = threeCups
-        threeCups.prev = destNode
         threeCups.next.next.next = prevNext
-        prevNext = threeCups.next.next
 
 nums = [int(num) for num in list(open(f'{sys.path[0]}/input.txt', 'r').readline())]
 nums.extend(list(range(max(nums) + 1, 1000001)))
